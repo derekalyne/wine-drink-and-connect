b5df919d03de1771d59b54626f89614a63f6c7d4
tanzhen li <tli67@illinois.edu>
2019-02-28 20:50:02 -0600

    route

--- wine/wine/urls.py
+++ wine/wine/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('wine/', include('wine.urls')),
+    path('welcome/', include('welcome.urls')),
     path('admin/', admin.site.urls),
 ]
