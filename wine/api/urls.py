5e0561b435366117aaf6cd536b90b512f6a3851f
tanzhen li <tli67@illinois.edu>
2019-03-28 14:16:44 -0500

    haha

--- wine/api/urls.py
+++ wine/api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('drinkers/', views.user_list),
-    path('drinkers/<str:username>', views.user_detail),
-    path('login/', views.user_login),
-    path('reviews/wid/<int:wid>', views.review_list),
-    path('reviews/rid/<int:rid>', views.review_update),
-    path('wines/', views.wine_list),
+    path('drinkers/', csrf_exempt(views.user_list),
+    path('drinkers/<str:username>', csrf_exempt(views.user_detail)),
+    path('login/', csrf_exempt(views.user_login)),
+    path('reviews/wid/<int:wid>', csrf_exempt(views.review_list)),
+    path('reviews/rid/<int:rid>', csrf_exempt(views.review_update)),
+    path('wines/', csrf_exempt(views.wine_list)),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
