2dbed678211fe495aaaca4ebfc7c2c70aed9e4fb
tanzhen li <tli67@illinois.edu>
2019-03-28 14:18:35 -0500

    asdasd

--- wine/api/urls.py
+++ wine/api/urls.py
@@ -4,7 +4,7 @@ from api import views
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('drinkers/', csrf_exempt(views.user_list),
+    path('drinkers/', csrf_exempt(views.user_list)),
     path('drinkers/<str:username>', csrf_exempt(views.user_detail)),
     path('login/', csrf_exempt(views.user_login)),
     path('reviews/wid/<int:wid>', csrf_exempt(views.review_list)),
