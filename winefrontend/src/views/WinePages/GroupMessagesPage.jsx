590d67a9e60e95e281c74fdee7690d894be5781a
augustg2 <naugust51@gmail.com>
2019-05-03 18:30:51 -0500

    added some styling to the messages pane

--- winefrontend/src/views/WinePages/GroupMessagesPage.jsx
+++ winefrontend/src/views/WinePages/GroupMessagesPage.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import classnames from "classnames";
 import WineContext from "../Context/wine-context"
 import { Link } from "react-router-dom";
-
-
+import { ChatFeed, Message } from 'react-chat-ui'
+import ExamplesNavbar from "./ExamplesNavbar";
 // reactstrap components
 import {
   Button,
   Container,
   NavLink,
   Row,
-  Col
+  Col,
 } from "reactstrap";
 
-// core components
-import ExamplesNavbar from "./ExamplesNavbar";
 
 function getCookie(name) {
   if (!document.cookie) {
@@ -30,22 +28,37 @@ function getCookie(name) {
   return decodeURIComponent(token[0].split('=')[1]);
 }
 
-const csrftoken = getCookie('csrftoken')
+const csrftoken = getCookie('csrftoken');
 
 class GroupMessagesPage extends React.Component {
+
+
+
+
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
   static contextType = WineContext;
-  state = {
-    messages: [],
-    name: "Group Name",
-    refreshInterval: null,
-  };
+    state = {
+        messages: [
+            new Message({
+                id: 1,
+                message: "Feel free to start a conversation!",
+            }), // Gray bubble
+            new Message({
+                id: 0,
+                message: "If you have any questions, ask the Wine! Team!"}), // Blue bubble
+        ],
+    };
   componentDidMount() {
     document.body.classList.toggle("group-messages");
     this.getMessages();
     // this.sendMessage(this.context.username);
-    // let self = this
-    // let interval = setInterval(self.getMessages, 2000);
-    // this.setState({refreshInterval: interval});
+    let self = this
+    let interval = setInterval(self.getMessages, 2000);
+    this.setState({refreshInterval: interval});
   }
   componentWillUnmount() {
     document.body.classList.toggle("group-messages");
@@ -53,8 +66,8 @@ class GroupMessagesPage extends React.Component {
   }
 
   getMessages = () =>{
-    // var url = `http://sp19-cs411-46.cs.illinois.edu:8000/api/message/` + this.context.group.mongoId;
-    var url = `http://127.0.0.1:8000/api/message/` + this.context.group.mongoId;
+    var url = `http://sp19-cs411-46.cs.illinois.edu:8000/api/message/` + this.context.group.mongoId;
+    // var url = `http://127.0.0.1:8000/api/message/` + this.context.group.mongoId;
     fetch(url,
         {
             method: "GET",
@@ -67,19 +80,30 @@ class GroupMessagesPage extends React.Component {
         })
         .catch( (e) => { return console.error("Error:", e) })
         .then( e => {
-          console.log(e)
+          console.log(e);
           if (e != null && e.length > 0){
-            let groupMessageObj = e[0]
-            this.setState({name: groupMessageObj.name})
-            this.setState({ messages: groupMessageObj.messages });
+              var test_messages  = [];
+              var counter = 0;
+              let groupMessageObj = e[0];
+              for(var i=0; i<groupMessageObj.messages.length; i++){
+                  let messageObj = new Message({
+                              id: counter,
+                              message: groupMessageObj.messages[i].text,
+                              senderName: this.context.username
+                          });
+                  counter += 1;
+                  test_messages.push(messageObj)
+              }
+              this.setState({name: groupMessageObj.name});
+              this.setState({ messages: test_messages });
           }
-        })  
-  }
+        })
+  };
 
 
   sendMessage = (text) => {
-    // var url = `http://sp19-cs411-46.cs.illinois.edu:8000/api/message/` + this.context.group.mongoId;
-    var url = `http://127.0.0.1:8000/api/message/` + this.context.group.mongoId;
+    var url = `http://sp19-cs411-46.cs.illinois.edu:8000/api/message/` + this.context.group.mongoId;
+    // var url = `http://127.0.0.1:8000/api/message/` + this.context.group.mongoId;
     var formData  = new FormData();
     formData.append("author",this.context.username)
     formData.append("text",text)
@@ -93,36 +117,54 @@ class GroupMessagesPage extends React.Component {
       }).then( (e) => {
           console.log(e);
       })
-  }
+  };
 
+  handleSubmit(event) {
+      event.preventDefault();
+      const data = new FormData(event.target);
+      console.log(data.get('messageBoxInput'))
+      this.sendMessage(data.get('messageBoxInput'))
+  }
 
   render() {
 
-    var messages = this.state.messages.map(m =>
-      <tr key = {m.sent_at} align='center'>
-          <td width="33%">{m.author}</td>
-          <td width="33%">{m.text}</td>
-          <td width="33%">{m.sent_at}</td>
-      </tr>
-    );
 
-    return (
+      return (
       <>
         <ExamplesNavbar />
         <div className="wrapper">
           <div className="page-header">
             <div className="page-header-image" />
             <div className="content">
-              <Container>
-              <table   align='center' >
-                <tr align='center'>
-                    <th width="33%"> Author</th>
-                    <th width="33%"> Text</th>
-                    <th width="33%"> Sent At</th>
-                </tr>
-                {messages}
-              </table>
-              </Container>
+
+                  <Container style={{width: '60%', height: '50%'}}>
+
+                      <ChatFeed
+                          style={{height: '50%'}}
+                          messages={this.state.messages} // Boolean: list of message objects
+                          //isTyping={this.state.is_typing} // Boolean: is the recipient typing
+                          hasInputField={false} // Boolean: use our input, or use your own
+                          showSenderName // show the name of the user who sent the message
+                          bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
+                          bubbleStyles={
+                              {
+                                  text: {
+                                      fontSize: 15
+                                  },
+                                  chatbubble: {
+                                      borderRadius: 30,
+                                      padding: 15
+                                  }
+                              }
+
+                          }
+
+                      />
+                      <form className="form-group" onSubmit={this.handleSubmit}>
+                          <input type="text" name="messageBoxInput" id="newMessage" placeholder="Type your message here:" className="form-control" />
+                          <Button>Submit</Button>
+                      </form>
+                  </Container>
             </div>
           </div>
         </div>
